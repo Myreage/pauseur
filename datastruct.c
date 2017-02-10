33aa21414714c5c73bd4f14366a9ac9c227eb50a
exBuried <r-hamel@outlook.fr>
2017-02-10 16:20:40 +0100

    Update 1

--- datastruct.c
+++ datastruct.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 char **createcolortable(int n){
+    int i;
     char **colortable=NULL;
     colortable=malloc(n*n*sizeof(char));
-
+    for (i=0;i<n;i++) colortable[i]=malloc(n*sizeof(char));
+    return colortable;
 }
 
 void freecolortable(char **colortable, int n){
-    int i,j;
+    int i;
     for (i=0;i<n;i++){
-        for (j=0;j<n;j++){
-            free(colortable[i][j]);
-        }
         free(colortable[i]);
     }
     free(colortable);
 }
 
-void fillcolortable(char **colortable, int n){
+void fillcolortablerand(char **colortable, int n){
     int i,j;
     char colors[6]={'B','V','R','J','M','G'};
     for (i=0;i<n;i++){
@@ -25,6 +24,17 @@ void fillcolortable(char **colortable, int n){
             colortable[i][j]=colors[rand()%6];
         }
     }
+}
+
+void fillcolortablefile(char **colortable, int n, char *filename){
+    FILE *file=fopen(filename, "r");
+    int i,j;
+    for (i=0;i<n;i++){
+        for (j=0;j<n;j++){
+            fscanf(file, "%c", &colortable[i][j]);
+        }
+    }
+    fclose(file);
 }
 
 int wintest(char **colortable, int n){
@@ -40,20 +50,34 @@ int wintest(char **colortable, int n){
 
 }
 
-/* Rajouter la fonction qui rempli la table a partir d'un fichier*/
-
 void switchcolor(char **colortable, char color, int x, int y) {
-    if (colortable[x][y] == NULL) printf("Error switch color\n");
+    if (colortable[x][y] == ' ') printf("Error switch color\n");
     else colortable[x][y] = color;
 }
 
-void updateconnexetab(char **colortable,char **connexetab, int n){ /* Je verrai bien un petit truc recursif */}
-
-void switchconnexecolors(char **colortable, char color, int x, int y){
-    char initcolor=colortable[0][0];
+void displaycolortable(char **colortable, int n){
+    int i,j;
+    for (i=0;i<n;i++){
+        for (j=0;j<n;j++){
+            printf("%c",colortable[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
+/*void updateconnexetab(char **colortable,char **connexetab, int n){*/ /* Je verrai bien un petit truc recursif }*/
+
+/*void switchconnexecolors(char **colortable, char color, int x, int y){
+    char initcolor=colortable[0][0];
+}*/
+
 int main(){
-    printf("lulz\n");
+    int n=12;
+    char **colortable=createcolortable(n);
+    fillcolortablerand(colortable, n);
+    displaycolortable(colortable, n);
+    switchcolor(colortable, 'V', 0, 0);
+    displaycolortable(colortable, n);
 }
 
