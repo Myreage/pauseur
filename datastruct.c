d6572dd9e3ab630a5167e752a6b95d91bc9e3725
exBuried <r-hamel@outlook.fr>
2017-02-10 17:22:29 +0100

    Update 2 : So close

--- datastruct.c
+++ datastruct.c
@@ -66,18 +66,45 @@ void displaycolortable(char **colortable, int n){
     printf("\n");
 }
 
-/*void updateconnexetab(char **colortable,char **connexetab, int n){*/ /* Je verrai bien un petit truc recursif }*/
 
-/*void switchconnexecolors(char **colortable, char color, int x, int y){
-    char initcolor=colortable[0][0];
-}*/
+int isconnexe(char **connexetable, int x, int y, int n){
+    int res;
+    if (connexetable[x][y]) res=1;
+    else if (x+1<n && connexetable[x+1][y]==1) res=1;
+    else if (x-1>0 && connexetable[x-1][y]==1) res=1;
+    else if (y+1<n && connexetable[x][y+1]==1) res=1;
+    else if (y-1>0 && connexetable[x][y-1]==1) res=1;
+    else res=0;
+    return res;
+}
+void updateconnexetab(char **colortable, char **connexetab, char color, int n){
+    /* Je pense que la fonction est à modifier dans le cas ou il y a un groupe de couleurs connexes ou peut être regler le problème
+     * avec un while style tant que (connexetable!=updateconnexetab(...)) (En changeant le type de la fonction) */
+    int i,j;
+    for (i=0; i<n;i++){
+        for (j=0; j<n; j++){
+            if (isconnexe(connexetab, i, j, n) && colortable[i][j]==color) connexetab[i][j]=1;
+        }
+    }
+}
+
+void switchconnexecolors(char **colortable, char **connexetable, char color, int n){
+    updateconnexetab(colortable, connexetable, color, n);
+    int i,j;
+    for (i=0; i<n;i++){
+        for (j=0; j<n; j++){
+            if (connexetable[i][j]==1) switchcolor(colortable, color, i, j);
+        }
+    }
+}
 
 int main(){
     int n=12;
     char **colortable=createcolortable(n);
     fillcolortablerand(colortable, n);
     displaycolortable(colortable, n);
-    switchcolor(colortable, 'V', 0, 0);
+    switchcolor(colortable, 'R', 0, 0);
     displaycolortable(colortable, n);
+    freecolortable(colortable, n);
 }
 
