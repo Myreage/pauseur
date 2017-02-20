60863a43f4a88cdf6d46f5703a1b89a59f110a07
exBuried <r-hamel@outlook.fr>
2017-02-20 15:18:36 +0100

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,7 +1,7 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-int wintest(int **colortable, int n);
+int wintest(int **connexetab, int n);
 int isconnexe(int **connexetable, int x, int y, int n);
 void updateconnexetab(char **colortable, int **connexetab, char color, int n);
 void switchconnexecolors(char **colortable, int **connexetab, char color, int n);
